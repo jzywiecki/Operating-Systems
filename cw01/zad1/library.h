4d50edc45e77a4f43591e70e3c6a033a73af97cf
jzywiecki <jakubzywiecki1@gmail.com>
2023-03-04 15:04:30 +0100

    Initial Commit

---
+++ cw01/zad1/library.h
@@ -0,0 +1,38 @@
+#ifndef ZAD1_LIBRARY_H
+#define ZAD1_LIBRARY_H
+
+#include <stdio.h>
+#include <libc.h>
+#include <stdbool.h>
+
+typedef struct {
+    char **pointers_array; //array of pointers
+    int size; //current size of pointers_array
+    int max_size; //max size of pointers_array
+} Counter;
+
+//function creating Counter structure
+bool create_counter(int max_size);
+
+//function removing block at given index in Counter structure
+bool remove_block(int index);
+
+//function adding block at given index in Counter structure
+bool add_block(char* block, int index);
+
+//function that free memory of Counter
+bool free_pointers_array();
+
+//function that gets block content at given index
+char* get_block_content(int index);
+
+//function that gets index of the first free space in pointers_array
+int get_free_index();
+
+//auxiliary function that creates command needed for our counting procedure
+char* create_command(char* file_name);
+
+//counting procedure for given file name
+void counting_procedure(Counter *structure, char* file_name);
+
+#endif //ZAD1_LIBRARY_H
