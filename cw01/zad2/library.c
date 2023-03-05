59314660e3036bade9413d7b4aad190af8987f0e
jzywiecki <jakubzywiecki1@gmail.com>
2023-03-06 00:24:20 +0100

    Fixed

---
+++ cw01/zad2/library.c
@@ -0,0 +1,189 @@
+#include "library.h"
+
+#include <stdio.h>
+#include <libc.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+Counter *counter;
+
+
+bool create_counter(int max_size){
+    if (max_size <= 0){ //check if max_size of our structure is at least 1
+        printf("Array length should be greater than 0!\n");
+        return false;
+    }
+    counter = malloc(sizeof(Counter)); //alloc memory for our structure
+    counter->max_size = max_size;
+    counter->size = 0;
+    counter->pointers_array = calloc(max_size, sizeof(char*)); //alloc memory for array of pointers
+    printf("Counter created successfully!\n");
+    return true;
+}
+
+
+bool remove_block(int index){
+    if (counter == NULL){ //check if structure exists
+        printf("Structure is not made yet!\n");
+        return false;
+    }
+    if( index < 0 || index >= counter->max_size) //check if index is proper
+    {
+        printf("Indexes of structure are above 0 and under max_size!\n");
+        return false;
+    }
+    if (counter->pointers_array[index] == NULL){ //check if there is block saved at given index
+        printf("There is no block saved at given index!\n");
+        return false;
+    }
+    if (counter->size == 0){ //check if array is empty
+        printf("Array is empty!\n");
+    }
+    counter->size--;
+    free(counter->pointers_array[index]);
+    counter->pointers_array[index] = NULL;
+    printf("Successfully removed block at given index!\n");
+    return true;
+}
+
+bool add_block(char* block, int index){
+    if (counter == NULL){ //check if structure exists
+        printf("Structure is not made yet!\n");
+        return false;
+    }
+    if( index < 0 || index >= counter->max_size)
+    {
+        printf("Indexes of structure are above 0 and under max_size!\n"); //check if index is proper
+        return false;
+    }
+    if (counter->size == counter->max_size){ //check if array is full
+        printf("Array is full!\n");
+    }
+    counter->size++;
+    counter->pointers_array[index] = (char *) calloc(strlen(block) + 1, sizeof(char));
+    strcpy(counter->pointers_array[index], block);
+    printf("Saved!\n");
+    return true;
+}
+
+bool free_pointers_array(){
+    if (counter == NULL){ //check if structure exists
+        printf("Structure is not made yet, so you can't delete it.\n");
+        return false;
+    }
+    for (int i = 0; i < counter->max_size; i++){ //free all pointers of pointers_array
+        if (counter->pointers_array[i] != NULL){
+            free(counter->pointers_array[i]);
+        }
+    }
+    free(counter->pointers_array); //free pointers_array
+    free(counter); //free the whole structure
+    printf("Action completed successfully!\n");
+    counter = NULL;
+    return true;
+}
+
+void get_block_content(int index){
+    if (counter == NULL || index < 0 || index >= counter->max_size || counter->pointers_array[index] == NULL){ //check if structure exists, index is proper and there is saved block at given idnex
+        printf("Incorrect action!\n");
+    }
+    else {
+        printf("%s\n", counter->pointers_array[index]);
+    }
+}
+
+int get_free_index(){
+    if (counter == NULL){ //check if structure exists
+        printf("Structure is not made yet!\n");
+        return -1;
+    }
+    if (counter->size == counter->max_size){ //check if array is full
+        printf("The array is full!\n");
+        return -1;
+    }
+    for (int i = 0; i < counter->max_size; i++){
+        if (counter->pointers_array[i] == NULL){
+            printf("Found free index (%d)!\n", i);
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+char* create_command(char* file_name){
+    char* beginning = "wc ";
+    char* tmp_file = " > tmp/fileAnalysis.tmp";
+
+    //count size of the whole command
+    unsigned long size = strlen(beginning) + strlen(tmp_file) + strlen(file_name) + 1;
+
+    //alloc size for the command
+    char* command = malloc(size);
+
+    //copy beginning to the command
+    strcpy(command, beginning);
+
+    //add file_name "wc file_name\0"
+    strcat(command, file_name);
+
+    //add tmp file we will save result at "wc file_name >> tmp/fileAnalysis.tmp\0"
+    strcat(command, tmp_file);
+
+    return command;
+}
+
+void counting_procedure(Counter *structure, char* file_name){
+    //check if structure is already made and if it has empty place to store new data
+    if (structure == NULL){
+        printf("Structure is not made yet.\n");
+        return;
+    }
+    if (structure->size == structure->max_size){
+        printf("Structure is already full.\n");
+        return;
+    }
+    if (access(file_name, F_OK) != 0){
+        printf("File does not exist!\n");
+        return;
+    }
+
+    //making tmp dir
+    system("mkdir tmp");
+
+    //running wc on file and saving it to tmp/fileAnalysis.tmp
+    system(create_command(file_name));
+
+    //opening file in read mode
+    FILE* file;
+    file = fopen("tmp/fileAnalysis.tmp", "r");
+
+    //here we count chars of our file analysis
+    int count_chars = 0;
+    int c; //fgetc() returns int
+    while ((c = fgetc(file))){
+        if (c == EOF){
+            break;
+        }
+        count_chars++;
+    }
+
+    //go back to file beginning
+    rewind(file);
+
+    //calloc memory for file content
+    char* file_content;
+    file_content = calloc(count_chars+1, sizeof(char));
+
+    //save file content to our char* file_content
+    fgets(file_content, count_chars, file);
+
+    //close file
+    fclose(file);
+
+    //removing tmp dir and files inside
+    system("rm -r -f tmp");
+
+    //adding block about our analysed file at free index
+    add_block(file_content, get_free_index());
+}
--- cw01/zad2/main.c
+++ cw01/zad2/main.c
@@ -1,6 +1,84 @@
-#include <stdio.h>
+#include "../zad1/library.h"
+#include <limits.h>
+#include <sys/times.h>
+
+
+Counter *counter;
+char* sep = " \n";
+
+void init_handler(char* operator){
+    char *end;
+    operator = strtok(NULL, sep);
+    int size = strtol(operator, &end, 10);
+    if (end==operator){
+        printf("Invalid init input!\n");
+    }
+    else {
+        create_counter(size);
+    }
+
+}
+
+void count_handler(char* operator){
+    operator = strtok(NULL, sep);
+    counting_procedure(counter, operator);
+}
+
+void show_handler(char* operator){
+    char *end;
+    operator = strtok(NULL, sep);
+    int index = strtol(operator, &end, 10);
+    if (end==operator){
+        printf("Invalid index input!\n");
+    }
+    else {
+        get_block_content(index);
+    }
+}
+
+void delete_handler(char* operator){
+    char *end;
+    operator = strtok(NULL, sep);
+    int index = strtol(operator, &end, 10);
+    if (end==operator){
+        printf("Invalid index input!\n");
+    }
+    else {
+        remove_block(index);
+    }
+}
+
+void destroy_handler(){
+    free_pointers_array();
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    char line[LINE_MAX];
+    while(1){
+        fgets(line, LINE_MAX, stdin);
+        char* operator = strtok(line, sep);
+
+        if (strcmp(operator, "init") == 0){
+            init_handler(operator);
+        }
+        else if (strcmp(operator, "count") == 0){
+            count_handler(operator);
+        }
+        else if (strcmp(operator, "show") == 0){
+            show_handler(operator);
+        }
+        else if (strcmp(operator, "delete") == 0){
+            delete_handler(operator);
+        }
+        else if (strcmp(operator, "destroy") == 0){
+            destroy_handler();
+        }
+        else if(strcmp(operator, "quit") == 0){
+            exit(0);
+        }
+        else{
+            printf("Command could not be recognised. Check spelling and try again!\n");
+        }
+    }
 }
+
