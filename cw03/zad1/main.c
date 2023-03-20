faf9056c8189211d1f21a94b4cc51a8c0c61cf3b
jzywiecki <jakubzywiecki1@gmail.com>
2023-03-20 02:37:10 +0100

    Finished lab3

--- cw03/zad1/main.c
+++ cw03/zad1/main.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
-
 int main(int argc, char * argv[]){
     if (argc < 2){
         printf("Not enaugh arguments!\n");
--- cw03/zad3/main.c
+++ cw03/zad3/main.c
@@ -4,61 +4,57 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 
 void recursive_call_directory(char* dir_path, char* string_to_find){
     DIR *dir = opendir(dir_path);
-    if (dir == NULL){
-        printf("Error entering directory!\n");
+    if (dir == NULL){ //katalog nie istnieje
+        perror("opendir");
         exit(EXIT_FAILURE);
     }
 
     struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL){
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) { //unikamy w ten sposob wchodzenia glebiej
+    while ((entry = readdir(dir)) != NULL){ //do czasu az mamy elementy w katalogu
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) { //unikamy w ten sposob wchodzenia glebiej, zgodnie ze wskazówką
             continue;
         }
 
-        char path[255];
-        snprintf(path, sizeof(path), "%s/%s", dir_path, entry->d_name); //konstrukcja sciezki do pliku/nastepnego folderu
+        char path[PATH_MAX];
+        snprintf(path, sizeof(path), "%s/%s", dir_path, entry->d_name); //konstrukcja sciezki do pliku lub nastepnego folderu
 
-        struct stat statbuf;
+        struct stat statbuf; //potrzebny do sprawdzania informacji na temat pliku/katalogu
         if (stat(path, &statbuf) == -1) {
-            printf("Error getting stat of path!\n");
+            perror("stat");
             continue;
         }
 
         if (S_ISDIR(statbuf.st_mode)){ //katalog
-            pid_t pid = fork(); //forkujemy
-            if (pid == -1){
-                printf("Error while forking!\n");
+            pid_t pid = fork(); //fork
+            if (pid == -1){ //obsluga bledu przy forkowaniu
+                perror("fork");
                 continue;
             }
 
-            if (pid == 0){ //dla nowo utworzonego procesu wchodzimy do tego katalogu
+            if (pid == 0){ //dla nowo utworzonego procesu wchodzimy do tego katalogu rekursywnie, w ten sposob uzyskamy drzewo katalogow i procesow
                 recursive_call_directory(path, string_to_find);
                 exit(EXIT_SUCCESS);
             }
         }
-        else if (S_ISREG(statbuf.st_mode)) { //plik
+        else if (S_ISREG(statbuf.st_mode)){ //plik
             FILE *file = fopen(path, "r");
-            if (file == NULL) {
-                printf("Could not open file!\n");
+            if (file == NULL){ //obsluga bledow przy otwarciu pliku
+                perror("fopen");
                 continue;
             }
 
             size_t line_len = strlen(string_to_find); //dlugosc sprawdzanego fragmentu
-            char *line = calloc(line_len + 1, sizeof(char)); //bufor do odczytania z pliku
+            char *line = calloc(line_len, sizeof(char)); //bufor do odczytania znaków z pliku
 
-            if (fread(line, sizeof(char), line_len, file) != 0) {
-                if (strncmp(line, string_to_find) == 0) {
+            if (fread(line, sizeof(char), line_len, file) != 0) { //czytamy linię z pliku, co wazne w przypadku gdy plik bedzie za krotki tu tez to obslugujemy
+                if (strcmp(line, string_to_find) == 0) {
                     printf("%s, %d\n", path, getppid()); //jesli fragmenty sa takie same wypisujemy sciezkie oraz pid
-                    break;
                 }
             }
-            else {
-                continue;
-            }
-
             //zwolnienie zasobow i zamkniecie plikow
             free(line);
             fclose(file);
@@ -70,10 +66,7 @@ void recursive_call_directory(char* dir_path, char* string_to_find){
         perror("closedir");
         exit(EXIT_FAILURE);
     }
-
 }
-
-
 
 int main(int argc, char* argv[]){
     if (argc < 3){
